43627a7743e45a85a8b57096e0f6558edb063391
Andey Robins <jtuttle5@uwyo.edu>
2022-09-26 16:43:55 -0600

    Add imperative option for MapReduce comparison

--- mapreduce/main.go
+++ mapreduce/main.go
@@ -20,10 +20,11 @@ func main() {
 	}
 
 	var in, out string
-	var help, wc, rbow bool
+	var help, wc, rbow, imp bool
 	flag.StringVar(&in, "in", "pass/common.txt", "the input file path")
 	flag.StringVar(&out, "out", "rainbow.csv", "the output file path")
 	flag.BoolVar(&rbow, "rbow", false, "use to generate the rainbow table")
+	flag.BoolVar(&imp, "imp", false, "run without mapreduce")
 	flag.BoolVar(&wc, "wc", false, "use to run wc test")
 	flag.BoolVar(&help, "help", false, "use to display help text")
 	flag.Parse()
@@ -42,6 +43,7 @@ func main() {
 		fmt.Println("  -in       The file name for password input. Defaults to pass/common.txt")
 		fmt.Println("  -out      The file name for the rainbow table. Defaults to rainbow.txt")
 		fmt.Println("  -wc       Run the word count map reduce program on julius-caesar.txt")
+		fmt.Println("  -imp      Run the rainbow table example in imperative mode (i.e. w/o MapReduce)")
 		fmt.Println("  -help     Display this help text :)")
 		pad()
 		return
@@ -51,6 +53,8 @@ func main() {
 		wordCount()
 	} else if rbow {
 		rainbow(in, out)
+	} else if imp {
+		imperative(in, out)
 	}
 
 }
@@ -92,4 +96,24 @@ func rainbow(in, out string) {
 			panic(err)
 		}
 	}).Run()
+}
+
+func imperative(in, out string) {
+	dat, err := os.ReadFile(in)
+	if err != nil {
+		panic(err)
+	}
+	output := ""
+	for _, line := range strings.Split(string(dat), "\n") {
+		if line != "" {
+			md5Hash := md5.Sum([]byte(line))
+			sha1Hash := sha1.Sum([]byte(line))
+			sha2Hash := sha256.Sum256([]byte(line))
+			sha3Hash := sha512.Sum512_256([]byte(line))
+
+			output = fmt.Sprintf("%v%v,%v,%v,%v,%v\n", output, line, hex.EncodeToString(md5Hash[:]), hex.EncodeToString(sha1Hash[:]), hex.EncodeToString(sha2Hash[:]), hex.EncodeToString(sha3Hash[:]))
+		}
+	}
+
+	os.WriteFile(out, []byte(output), 0644)
 }
